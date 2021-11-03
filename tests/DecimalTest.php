99520fa9387cd93f661f506a09253bfa9d3fd4d0
kameronramah <kramaherison@gmail.com>
2021-11-03 14:24:19 +0100

    Premier commit

---
+++ tests/DecimalTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class DecimalTest extends TestCase
+{
+    public function testParam()
+    {
+        $this->assertEquals('La valeur spécifiée n\'est pas comprise entre 0 et 3000', Decimal::decimalToRoman(-1));
+    }
+}

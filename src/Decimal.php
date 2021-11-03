78c913b7c1cb83e486adadbf51172e8393c7ce70
kameronramah <kramaherison@gmail.com>
2021-11-03 14:25:26 +0100

    Test paramètre négatif

--- src/Decimal.php
+++ src/Decimal.php
@@ -5,6 +5,9 @@ class Decimal
     
     public static function decimalToRoman(int $param): string 
     {
+        if($param < 0) {
+            return 'La valeur spécifiée n\'est pas comprise entre 0 et 3000';
+        }
        return '';
     }
 
